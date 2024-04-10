bffafddb1b0c04c8b01a0eb2abf713c6cc46cb84
tphelanECON <46010380+tphelanECON@users.noreply.github.com>
2024-04-10 09:54:48 -0400

    Add files via upload

--- code/analysis.py
+++ code/analysis.py
@@ -1,36 +1,30 @@
 """
 This script performs the analysis for the Commentary, using the data constructed in data_clean.py.
 
-Only four figures now I think?
-
-It creates 14 figures and 4 tables.
+Tables:
+    * Summary tables giving median and means income and net worth for student debtors, the whole population,
+    and families in which the respondent is under the age of 35.
 
 Figures:
     * Average student debt by quintiles, broken down by:
-        * income and net worth quintiles
-        * whole population and population of debtors
+        * income and net worth quintiles.
+        * whole population and population of debtors.
     * Incidence of debt by quintiles of income and net worth.
     * Ratio of conditional mean of student debt to conditional median of student debt
     by quintiles of income and net worth.
-
-Tables:
-    * Summary tables for student debtors and whole population (means and medians of income and net worth)
-    * Fraction of debt in repayment by type (federal versus private) and reason for non-repayment
 """
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-# import matplotlib as mpl
-# import time, datetime, pyreadstat, sys, os
+import matplotlib as mpl
 import data_clean
-from warnings import simplefilter
 
 """
 Set dummy to indicate if plots are shown and suppress warning (from https://github.com/twopirllc/pandas-ta/issues/340)
 """
 
 show = 0
-simplefilter(action="ignore", category=pd.errors.PerformanceWarning)
 
 """
 Import the data, functions and lists that we need
@@ -52,13 +46,20 @@ weight_mean_df = data_clean.weight_mean_df
 weight_agg_df = data_clean.weight_agg_df
 
 # Plots
-c1, c2 = data_clean.c1, data_clean.c2
-colorfader = data_clean.colorfader
+c1 = 'crimson'
+c2 = 'darkblue'
+
+
+def colorfader(color1, color2, mix):
+    return mpl.colors.to_hex(
+        (1 - mix) * np.array(mpl.colors.to_rgb(color1)) + mix * np.array(mpl.colors.to_rgb(color2)))
+
+
 name_dict = {'income': 'Income', 'networth': 'Net worth'}
 xlabelfontsize = 15
 ylabelfontsize = 15
 titlefontsize = 15
-width = 1/5
+width = 1 / 5
 
 """
 Summary tables (for the "summary statistics" section)
@@ -79,12 +80,11 @@ for yr in [2019, 2022]:
             df.iloc[2, i] = weight_median_df(d, 'networth') / 10 ** 3
             df.iloc[3, i] = weight_mean_df(d, 'networth') / 10 ** 3
 
-        destin = '../main/figures/sd_summary_{0}_{1}.tex'.format(yr, demo_var)
+        destin = '../main/figures/summary_{0}_{1}.tex'.format(yr, demo_var)
         df_table = df.round(decimals=1)
         with open(destin, 'w') as tf:
             df_table = df_table.style.format(precision=1)
             tf.write(df_table.to_latex(column_format='lccc'))
-
 
 """
 Average (mean) student debt by quintiles. Indexed by:
@@ -95,119 +95,108 @@ Therefore 2 x 2 = 4 figures.
 """
 
 for var in ['income', 'networth']:
-    for debt_var in ['SD']:
-        for pop_var in ['student debtors', 'all']:
-            SD_quintiles = pd.DataFrame(columns=range(1, 6), index=[2019, 2022])
-            for yr in [2019, 2022]:
-                if pop_var == 'student debtors':
-                    df_temp = scf_debtors[yr]
-                else:
-                    df_temp = scf[yr]
-                f = lambda x: np.average(x, weights=df_temp.loc[x.index, "wgt"])
-                SD_quintiles.loc[yr, :] = df_temp.groupby(var+'_cat5')[debt_var].agg(f).values
-            SD_quintiles = (SD_quintiles/1000).astype(float).round(1)
-            fig = plt.figure()
-            ax = fig.add_subplot(111)
-            for i in range(5):
-                if i == 0:
-                    ax.bar(i + 1 - width, SD_quintiles.loc[2019, i + 1], 2 * width, color=colorfader(c1, c2, 0),
-                           label=2019)
-                    ax.bar(i + 1 + width, SD_quintiles.loc[2022, i + 1], 2 * width, color=colorfader(c1, c2, 1),
-                           label=2022)
-                else:
-                    ax.bar(i + 1 - width, SD_quintiles.loc[2019, i + 1], 2 * width, color=colorfader(c1, c2, 0))
-                    ax.bar(i + 1 + width, SD_quintiles.loc[2022, i + 1], 2 * width, color=colorfader(c1, c2, 1))
-            plt.legend()
-            ax.set_xlabel('{0} quintile'.format(name_dict[var]), fontsize=xlabelfontsize)
-            ax.set_ylabel('Thousands', fontsize=ylabelfontsize)
-            if debt_var == 'SD':
-                if pop_var == 'all':
-                    ax.set_title('Average student debt', fontsize=titlefontsize)
-                else:
-                    ax.set_title('Average student debt among student debtors', fontsize=titlefontsize)
+    for pop_var in ['student debtors', 'all']:
+        SD_quintiles = pd.DataFrame(columns=range(1, 6), index=[2019, 2022])
+        for yr in [2019, 2022]:
+            if pop_var == 'student debtors':
+                df_temp = scf_debtors[yr]
             else:
-                if pop_var == 'all':
-                    ax.set_title('Average private student debt', fontsize=titlefontsize)
-                else:
-                    ax.set_title('Average private student debt among student debtors', fontsize=titlefontsize)
-            destin = '../main/figures/{0}_ave_{1}_{2}.png'.format(var[:3], debt_var[:4], pop_var[:3])
-            plt.savefig(destin, format='png', dpi=1000)
-            if show == 1:
-                plt.show()
-            plt.close()
+                df_temp = scf[yr]
+            f = lambda x: np.average(x, weights=df_temp.loc[x.index, "wgt"])
+            SD_quintiles.loc[yr, :] = df_temp.groupby(var + '_cat5')['SD'].agg(f).values
+        SD_quintiles = (SD_quintiles / 1000).astype(float).round(1)
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        for i in range(5):
+            if i == 0:
+                ax.bar(i + 1 - width, SD_quintiles.loc[2019, i + 1], 2 * width, color=colorfader(c1, c2, 0),
+                       label=2019)
+                ax.bar(i + 1 + width, SD_quintiles.loc[2022, i + 1], 2 * width, color=colorfader(c1, c2, 1),
+                       label=2022)
+            else:
+                ax.bar(i + 1 - width, SD_quintiles.loc[2019, i + 1], 2 * width, color=colorfader(c1, c2, 0))
+                ax.bar(i + 1 + width, SD_quintiles.loc[2022, i + 1], 2 * width, color=colorfader(c1, c2, 1))
+        plt.legend()
+        ax.set_xlabel('{0} quintile'.format(name_dict[var]), fontsize=xlabelfontsize)
+        ax.set_ylabel('Thousands', fontsize=ylabelfontsize)
+        if pop_var == 'all':
+            ax.set_title('Average student debt', fontsize=titlefontsize)
+        else:
+            ax.set_title('Average student debt among student debtors', fontsize=titlefontsize)
+        destin = '../main/figures/{0}_ave_{1}.png'.format(var[:3], pop_var[:3])
+        plt.savefig(destin, format='png', dpi=1000)
+        if show == 1:
+            plt.show()
+        plt.close()
 
 """
 Percentage of families with student debt by quintiles of income and net worth.
 """
 
 for var in ['income', 'networth']:
-    for debt_var in ['SD']:
-        SD_quintiles_frac = pd.DataFrame(columns=range(1, 6), index=[2019, 2022])
-        for yr in [2019, 2022]:
-            data = scf[yr]
-            quintiles = np.array([quantile(data[var], data['wgt'], j/5) for j in range(6)])
-            qct_lists, var_names = [quintiles, [0, 1, np.inf]], [var, debt_var]
-            d = [pd.cut(data[var_names[i]], bins=qct_lists[i], labels=range(len(qct_lists[i])-1),
-                        include_lowest=True, duplicates='drop') for i in range(2)]
-            data['pairs'] = list(zip(d[0], d[1]))
-            SD_debt = data.groupby(data['pairs'])[debt_var]
-            SD_debt_count = data.groupby(data['pairs'])['wgt'].sum()
-            SD_quintiles_frac.loc[yr, :] = [SD_debt_count[(i, 1)]/(SD_debt_count[(i, 0)]+SD_debt_count[(i, 1)]) for i in range(5)]
-        SD_quintiles_pct = 100*SD_quintiles_frac
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        for i in range(5):
-            if i == 0:
-                ax.bar(i+1-width, SD_quintiles_pct.loc[2019, i+1], 2*width, color=colorfader(c1, c2, 0), label=2019)
-                ax.bar(i+1+width, SD_quintiles_pct.loc[2022, i+1], 2*width, color=colorfader(c1, c2, 1), label=2022)
-            else:
-                ax.bar(i+1-width, SD_quintiles_pct.loc[2019, i+1], 2*width, color=colorfader(c1, c2, 0))
-                ax.bar(i+1+width, SD_quintiles_pct.loc[2022, i+1], 2*width, color=colorfader(c1, c2, 1))
-        plt.legend()
-        ax.set_xlabel('{0} quintile'.format(name_dict[var]), fontsize=xlabelfontsize)
-        ax.set_ylabel('Percent', fontsize=ylabelfontsize)
-        if debt_var == 'SD':
-            ax.set_title('Percentage of families with student debt', fontsize=titlefontsize)
-            ax.set_ylim([0, 50])
+    SD_quintiles_frac = pd.DataFrame(columns=range(1, 6), index=[2019, 2022])
+    for yr in [2019, 2022]:
+        data = scf[yr]
+        quintiles = np.array([quantile(data[var], data['wgt'], j / 5) for j in range(6)])
+        qct_lists, var_names = [quintiles, [0, 1, np.inf]], [var, 'SD']
+        d = [pd.cut(data[var_names[i]], bins=qct_lists[i], labels=range(len(qct_lists[i]) - 1),
+                    include_lowest=True, duplicates='drop') for i in range(2)]
+        data['pairs'] = list(zip(d[0], d[1]))
+        SD_debt = data.groupby(data['pairs'])['SD']
+        SD_debt_count = data.groupby(data['pairs'])['wgt'].sum()
+        SD_quintiles_frac.loc[yr, :] = [SD_debt_count[(i, 1)] / (SD_debt_count[(i, 0)] + SD_debt_count[(i, 1)]) for i in
+                                        range(5)]
+    SD_quintiles_pct = 100 * SD_quintiles_frac
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    for i in range(5):
+        if i == 0:
+            ax.bar(i + 1 - width, SD_quintiles_pct.loc[2019, i + 1], 2 * width, color=colorfader(c1, c2, 0), label=2019)
+            ax.bar(i + 1 + width, SD_quintiles_pct.loc[2022, i + 1], 2 * width, color=colorfader(c1, c2, 1), label=2022)
         else:
-            ax.set_title('Percentage with private student debt', fontsize=titlefontsize)
-        destin = '../main/figures/{0}_pct_{1}.png'.format(var[:3], debt_var[:4])
-        plt.savefig(destin, format='png', dpi=1000)
-        if show == 1:
-            plt.show()
-        plt.close()
+            ax.bar(i + 1 - width, SD_quintiles_pct.loc[2019, i + 1], 2 * width, color=colorfader(c1, c2, 0))
+            ax.bar(i + 1 + width, SD_quintiles_pct.loc[2022, i + 1], 2 * width, color=colorfader(c1, c2, 1))
+    plt.legend()
+    ax.set_xlabel('{0} quintile'.format(name_dict[var]), fontsize=xlabelfontsize)
+    ax.set_ylabel('Percent', fontsize=ylabelfontsize)
+    ax.set_title('Percentage of families with student debt', fontsize=titlefontsize)
+    ax.set_ylim([0, 50])
+    destin = '../main/figures/{0}_pct.png'.format(var[:3])
+    plt.savefig(destin, format='png', dpi=1000)
+    if show == 1:
+        plt.show()
+    plt.close()
 
 """
 Ratio of conditional mean to conditional median of student debt. 
 """
 
-for debt_var in ['SD']:
-    for var in ['income', 'networth']:
-        SD_quintiles_rat = pd.DataFrame(columns=range(1, 6), index=[2019, 2022])
-        for yr in [2019, 2022]:
-            data = scf_debtors[yr]
-            f = lambda x: np.average(x, weights=data.loc[x.index, "wgt"])
-            g = lambda x: weight_median(x, weights=data.loc[x.index, "wgt"])
-            gb = data.groupby(var+'_cat{0}'.format(5))[debt_var].agg(f).values
-            gb_med = data.groupby(var+'_cat{0}'.format(5))[debt_var].agg(g).values
-            SD_quintiles_rat.loc[yr, :] = gb/gb_med
+for var in ['income', 'networth']:
+    SD_quintiles_rat = pd.DataFrame(columns=range(1, 6), index=[2019, 2022])
+    for yr in [2019, 2022]:
+        data = scf_debtors[yr]
+        f = lambda x: np.average(x, weights=data.loc[x.index, "wgt"])
+        g = lambda x: weight_median(x, weights=data.loc[x.index, "wgt"])
+        gb = data.groupby(var + '_cat{0}'.format(5))['SD'].agg(f).values
+        gb_med = data.groupby(var + '_cat{0}'.format(5))['SD'].agg(g).values
+        SD_quintiles_rat.loc[yr, :] = gb / gb_med
 
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        for i in range(5):
-            if i == 0:
-                ax.bar(i+1-width, SD_quintiles_rat.loc[2019, i+1], 2*width, color=colorfader(c1, c2, 0), label=2019)
-                ax.bar(i+1+width, SD_quintiles_rat.loc[2022, i+1], 2*width, color=colorfader(c1, c2, 1), label=2022)
-            else:
-                ax.bar(i+1-width, SD_quintiles_rat.loc[2019, i+1], 2*width, color=colorfader(c1, c2, 0))
-                ax.bar(i+1+width, SD_quintiles_rat.loc[2022, i+1], 2*width, color=colorfader(c1, c2, 1))
-        plt.legend()
-        ax.set_xlabel('{0} quintile'.format(name_dict[var]), fontsize=xlabelfontsize)
-        ax.set_ylabel('Ratio', fontsize=ylabelfontsize)
-        ax.set_ylim([0, 2.2])
-        ax.set_title('Ratio of mean-to-median student debt', fontsize=titlefontsize)
-        destin = '../main/figures/{0}_MM.png'.format(var[:3])
-        plt.savefig(destin, format='png', dpi=1000)
-        if show == 1:
-            plt.show()
-        plt.close()
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    for i in range(5):
+        if i == 0:
+            ax.bar(i + 1 - width, SD_quintiles_rat.loc[2019, i + 1], 2 * width, color=colorfader(c1, c2, 0), label=2019)
+            ax.bar(i + 1 + width, SD_quintiles_rat.loc[2022, i + 1], 2 * width, color=colorfader(c1, c2, 1), label=2022)
+        else:
+            ax.bar(i + 1 - width, SD_quintiles_rat.loc[2019, i + 1], 2 * width, color=colorfader(c1, c2, 0))
+            ax.bar(i + 1 + width, SD_quintiles_rat.loc[2022, i + 1], 2 * width, color=colorfader(c1, c2, 1))
+    plt.legend()
+    ax.set_xlabel('{0} quintile'.format(name_dict[var]), fontsize=xlabelfontsize)
+    ax.set_ylabel('Ratio', fontsize=ylabelfontsize)
+    ax.set_ylim([0, 2.2])
+    ax.set_title('Ratio of mean-to-median student debt', fontsize=titlefontsize)
+    destin = '../main/figures/{0}_MM.png'.format(var[:3])
+    plt.savefig(destin, format='png', dpi=1000)
+    if show == 1:
+        plt.show()
+    plt.close()
--- code/data_clean.py
+++ code/data_clean.py
@@ -1,91 +1,23 @@
 """
 This script downloads the 2019 and 2022 waves of the SCF and generates variables
-used in the analysis if such files do not already exist in memory.
+used in the Commentary if such files do not already exist in memory.
 
-It also prints some checks (comparisons with the official report) and prints some numbers
-that are used in the main text.
+It also prints some figures used in the main text.
 
-Documentation links for the 2022 wave released in October 2023:
-
-    * Official report (previously referred to the Bulletin) found at:
-    https://www.federalreserve.gov/publications/files/scf23.pdf
-    * Summary macros: https://www.federalreserve.gov/econres/files/bulletin.macro.txt
-    * Codebook for 2022: https://www.federalreserve.gov/econres/files/codebk2022.txt
-
-With each new wave of the SCF the Board updates the previous summary datasets
-to be in current dollars but does NOT do this with the full public dataset.
-From page 36 of the Report we adjust nominal figures in the full public dataset as follows:
-
-For 2019:
-    * Adjustment factor for assets and debts in survey year = 1.1592
-    * Adjustment factor for income in year before survey year = 1.1802
-
-For 2022:
-    * Adjustment factor for assets and debts in survey year = 1.0000
-    * Adjustment factor for income in year before survey year = 1.0809
-
-Reminders/caveats/concerns:
-    * edn_inst in the summary dataset differs from what we obtain when
-    summing over the first six loans. However, the difference is negligible and simply noted in a footnote.
-    * The SCF asked respondents about the income they earned from the PREVIOUS year. The respondent is
-    therefore providing a figure denominated in the previous year's units. It is for this reason that we must
-    adjust as per the above factor. This is NOT necessary for the summary dataset.
-    * quantiles must be calculated BEFORE any conditioning by debt.
-    i.e. when we plot things like average debt among debtors per quintile of
-    the distribution of, e.g. net worth, these are not quintiles of the DEBTOR
-    population but rather quintiles of the WHOLE population.
-    * name_dict is dictionary that relabels variables used in the figures.
-    * Per-capita: divide quantities by 2 if married==1. Summary macros: married=1
-    if respondent is married or living with their partner.
-    NOTE THAT THE CURRENT COMMENTARY DOES NOT USE PER-CAPITA FIGURES.
-    * In the public dataset, the PSLF is combined with the forbearance category.
-    * Student debt is labelled 'edn_inst' in the summary SCF dataset.
-
-Nothing in the current Commentary requires anything from the loan-level information. This is ALL aggregates.
+Authors: Tom Phelan and Emily Moschini.
+Date written: April 2024.
 """
+
 
 import numpy as np
 import pandas as pd
-import matplotlib as mpl
+# import matplotlib as mpl
 import os
 from io import BytesIO
 # from zipfile import ZipFile
 # from urllib.request import urlopen
 import requests
 import zipfile
-from warnings import simplefilter
-
-"""
-TP: suppress performance warnings. See: https://stackoverflow.com/questions/68292862/
-"""
-
-simplefilter(action="ignore", category=pd.errors.PerformanceWarning)
-
-"""
-Questions and variables from full public dataset. Recall SCF only solicits
-loan-level info for the first six loans. Also recall that x42001 are weights
-(divided by 5 to be consistent with "wgt" in summary dataset).
-"""
-
-# Q1. How much is still owed on this loan? (x7179 represents "all other loans")
-# Key: 0. NA; otherwise dollar amount
-bal_list = ['x7824', 'x7847', 'x7870', 'x7924', 'x7947', 'x7970', 'x7179']
-# Q3. Is this loan a federal student loan such as Stafford, Direct, PLUS, or Perkins?
-# Key: 1. *YES; 5. *NO; 0. NA
-fed_list = ['x7879', 'x7884', 'x7889', 'x7894', 'x7899', 'x7994']
-# Q4. (Are you/Is he/Is she/Is he or she) making payments on this loan now?
-# Key: 1. *YES; 5. *NO; 0. NA
-paynow_list = ['x7806', 'x7829', 'x7852', 'x7906', 'x7929', 'x7952']
-# Q5. What is the reason that (you are/he is/she is/he or she is) not making payments
-# on (your/his/her/his or her) loan? (Are you/Is he/Is she/Are they) in
-# forebearance, a post-graduation grace period, in a job or public service related
-# loan forgiveness program, or simply unable to afford the loan payment?
-# Key: 1. IN FORBEARANCE; 2. JOB OR PUBLIC SERVICE LOAN FORGIVENESS PROGRAM;
-# 3. UNABLE TO AFFORD LOAN PAYMENT; 4. POST-GRADUATION GRACE PERIOD OR STILL
-# ENROLLED -7. OTHER 0. Inap.
-whynopay_list = ['x9300', 'x9301', 'x9302', 'x9303', 'x9304', 'x9305']
-
-full_list = ['yy1', 'y1', 'x42001'] + bal_list + fed_list + paynow_list + whynopay_list
 
 """
 Make folder for figures and data if these do not exist
@@ -168,21 +100,8 @@ def cut(df, var, qctiles):
     return pd.cut(df[var], bins=qctiles, labels=range(len(qctiles)-1), include_lowest=True, duplicates='drop')
 
 
-# 'aliceblue'
-c1 = 'crimson'
-c2 = 'darkblue'
-
-
-def colorfader(color1, color2, mix):
-    return mpl.colors.to_hex((1-mix)*np.array(mpl.colors.to_rgb(color1)) + mix*np.array(mpl.colors.to_rgb(color2)))
-
-
 """
-Download data. Only need the summary dataset. 
-"""
-
-"""
-Define years used and dictionaries for the two datasets.
+Download data and define dictionary used.  
 """
 
 years = [2019, 2022]
@@ -198,7 +117,7 @@ for yr in years:
     else:
         print("No file exists for {0} wave. Now downloading.".format(yr))
         """
-        Get summary dataset (main source used in Commentary). Label scf_sum.
+        Get summary dataset (only data used in the Commentary)
         """
         url = 'https://www.federalreserve.gov/econres/files/scfp{0}s.zip'.format(yr)
         scf[yr] = data_from_url(url)
@@ -238,56 +157,36 @@ Statistics mentioned in introduction and first part of the analysis.
 """
 
 """
-Means and median
+Means and median quoted in introduction
 """
 
-for yr in [2019]:
+for yr in [2019, 2022]:
     print("Year = {0}:".format(yr))
-    print("Median student debt:")
-    print("Among student debtors:", weight_median_df(scf_debtors[yr], 'SD'))
-    print("Among <35 population of student debtors:", weight_median_df(scf_young_debtors[yr], 'SD'))
-    print("Mean student debt:")
-    print("Whole population:", weight_mean_df(scf[yr], 'SD'))
-    print("Among student debtors:", weight_mean_df(scf_debtors[yr], 'SD'))
-    print("Among <35 population:", weight_mean_df(scf_young[yr], 'SD'))
-    print("Among <35 population with student debt:", weight_mean_df(scf_young_debtors[yr], 'SD'))
+    print("Median student debt among student debtors:", round(weight_median_df(scf_debtors[yr], 'SD')/10**3, 2), "thousands")
+    print("Mean student debt (whole population):", round(weight_mean_df(scf[yr], 'SD')/10**3, 2), "thousands")
+    print("Mean student debt (student debtors):", round(weight_mean_df(scf_debtors[yr], 'SD')/10**3, 2), "thousands")
 
-print("Incidence")
+"""
+Incidence, aggregates, and ages (quoted in the main text in section 2.1)
+"""
+
+print("Incidence and aggregates")
 
 for yr in [2019, 2022]:
     print("Year = {0}:".format(yr))
-    print("Incidence in whole population:", weight_agg_df(scf_debtors[yr], 'wgt')/weight_agg_df(scf[yr], 'wgt'))
-    print("Incidence in <35 population:", weight_agg_df(scf_young_debtors[yr], 'wgt')/weight_agg_df(scf_young[yr], 'wgt'))
-
-print("Aggregates")
-
-for yr in [2019, 2022]:
-    print("Year = {0}:".format(yr))
+    print("Incidence (percentange) in whole population:",
+          round(100*weight_agg_df(scf_debtors[yr], 'wgt')/weight_agg_df(scf[yr], 'wgt'), 2))
+    print("Aggregate student debt in SCF:", round(weight_agg_df(scf[yr], 'SD')/10**12, 2), "trillion")
     print("As percent of agg income:", round(100*weight_agg_df(scf[yr], 'SD')/weight_agg_df(scf[yr], 'income'), 2))
     print("As percent of agg net worth:", round(100*weight_agg_df(scf[yr], 'SD')/weight_agg_df(scf[yr], 'networth'), 2))
-    print("As percent of agg income (<35 population):", round(100*weight_agg_df(scf_young[yr], 'SD')/weight_agg_df(scf_young[yr], 'income'), 2))
-    print("As percent of agg net worth (<35 population):", round(100*weight_agg_df(scf_young[yr], 'SD')/weight_agg_df(scf_young[yr], 'networth'), 2))
-
-print("Young versus old split")
-
-for yr in [2019, 2022]:
-    print("Year = {0}:".format(yr))
-    print("Percent of student debt held by <35 population:",
-          round(100*weight_agg_df(scf_young[yr], 'SD')/weight_agg_df(scf[yr], 'SD'), 2))
-    print("Percent of families <35:",
-          round(100 * weight_agg_df(scf_young[yr], 'wgt') / weight_agg_df(scf[yr], 'wgt'), 2))
-
-"""
-
-"""
-
 
 """
 Ages (quoted in the main text in section 2.1)
 """
 
+print("Ages")
+
 for yr in [2019, 2022]:
     print("Median ages for year = {0}:".format(yr))
     print("Whole population:", weight_median(scf[yr]['age'], scf[yr]['wgt']))
     print("Student debtors:", weight_median(scf_debtors[yr]['age'], scf_debtors[yr]['wgt']))
-
